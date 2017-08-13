3d7d07a1f0239bc7174026a99ba439181040f13f
ssc <ssc.open@googlemail.com>
2017-08-13 19:31:06 +0200

    added missing items to rescore

--- experiments/bookcrossing.sh
+++ experiments/bookcrossing.sh
@@ -1,1 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+NUM_REPETITIONS=10
+EXECUTION_TIME=$(date +%d_%m_%Y_%H_%M);
+
+for i in $(seq 1 $NUM_REPETITIONS);
+do
+  cargo run --release datasets/bookcrossing-shuffled.csv 445802 105279 8|tee logs/bookcrossing-$EXECUTION_TIME-$i.txt
+  grep ms logs/bookcrossing-$EXECUTION_TIME-$i.txt|grep -v Overall|cut -d' ' -f2|sed s/ms//g > logs/bookcrossing-$EXECUTION_TIME-$i.csv
+done
--- experiments/movielens.sh
+++ experiments/movielens.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-NUM_REPETITIONS=1
+NUM_REPETITIONS=10
 EXECUTION_TIME=$(date +%d_%m_%Y_%H_%M);
 
 for i in $(seq 1 $NUM_REPETITIONS);
 do
-  #cargo run --release ../datasets/ml1m-shuffled.csv 9746 6040 8|tee logs/movielens-$EXECUTION_TIME-$i.txt
-  grep ms logs/movielens-$EXECUTION_TIME-$i.txt|grep -V Overall
+  cargo run --release datasets/ml1m-shuffled.csv 9746 6040 8|tee logs/movielens-$EXECUTION_TIME-$i.txt
+  grep ms logs/movielens-$EXECUTION_TIME-$i.txt|grep -v Overall|cut -d' ' -f2|sed s/ms//g|head -n 100 > logs/movielens-$EXECUTION_TIME-$i.csv
 done
