af0c6db3f89ab29f04d29e8227dd95a61a5016c5
sscdotopen <ssc.open@googlemail.com>
2018-05-21 16:18:11 +0200

    Added benchmark for precomputation of logarithms

--- benches/llr.rs
+++ benches/llr.rs
@@ -5,9 +5,10 @@ extern crate rand;
 use bencher::Bencher;
 use rand::{Rng,SeedableRng,XorShiftRng};
 
+
 #[inline(always)]
 fn x_log_x(x: u64) -> f64 {
-  if x == 0 { 0.0 } else { (x as f64) * (x as f64).ln() }
+  if x == 0 { 0.0 } else { x as f64 * (x as f64).ln() }
 }
 
 #[inline(always)]
@@ -21,7 +22,6 @@ fn llr_cse(k11: u64, k12: u64, k21: u64, k22: u64) -> f64 {
   let matrix_entropy = xlx_all - x_log_x(k11) - x_log_x(k12) -  x_log_x(k21) - x_log_x(k22);
 
   if row_entropy + column_entropy < matrix_entropy {
-    // round off error
     0.0
   } else {
     2.0 * (row_entropy + column_entropy - matrix_entropy)
@@ -50,10 +50,73 @@ fn entropy4(a: u64, b: u64, c: u64, d: u64) -> f64 {
   x_log_x(a + b + c + d) - x_log_x(a) - x_log_x(b) - x_log_x(c) - x_log_x(d)
 }
 
+#[inline(always)]
+pub fn llr_cse_pre(k11: u64, k12: u64, k21: u64, k22: u64, logs: &Vec<f64>) -> f64 {
+
+  let xlx_all = x_log_x(k11 + k12 + k21 + k22);
+
+  let log_k11 = logs[k11 as usize];
+  let log_k12 = logs[k12 as usize];
+  let log_k21 = logs[k21 as usize];
+  let log_k11_12 = logs[(k11 + k12) as usize];
+  let log_k11_21 = logs[(k11 + k21) as usize];
+
+  let row_entropy = xlx_all - x_times_log_x(k11 + k12, log_k11_12) - x_log_x(k21 + k22);
+  let column_entropy = xlx_all - x_times_log_x(k11 + k21, log_k11_21) - x_log_x(k12 + k22);
+  let matrix_entropy = xlx_all - x_times_log_x(k11, log_k11) - x_times_log_x(k12, log_k12) -
+      x_times_log_x(k21, log_k21) - x_log_x(k22);
+
+  if row_entropy + column_entropy < matrix_entropy {
+    0.0
+  } else {
+    2.0 * (row_entropy + column_entropy - matrix_entropy)
+  }
+}
+
+#[inline(always)]
+fn x_times_log_x(x: u64, log_x: f64) -> f64 {
+  x as f64 * log_x
+}
+
+pub fn pre_compute_logarithms(max_cooccurrences: usize) -> Vec<f64> {
+
+  let mut pre_computed_logarithms: Vec<f64> =
+    std::iter::repeat(0.0)
+        .take(max_cooccurrences)
+        .collect::<Vec<f64>>();
+
+  for index in 1..max_cooccurrences {
+    pre_computed_logarithms[index] = (index as f64).ln();
+  }
+
+  pre_computed_logarithms
+}
+
+
 
 const NUM_REPETITIONS: u32 = 1000;
 const FIXED_SEED: [u32; 4] = [1, 2, 3, 4];
+const MAX_OBSERVATIONS: u64 = 500;
+const MAX_COOCCURRENCES: u64 = MAX_OBSERVATIONS * MAX_OBSERVATIONS;
 
+
+
+fn bench_llr_cse_pre(bench: &mut Bencher) {
+
+  let mut rng = XorShiftRng::from_seed(FIXED_SEED);
+
+  let logs = pre_compute_logarithms((MAX_COOCCURRENCES + MAX_OBSERVATIONS) as usize);
+
+  bench.iter(|| {
+    for _ in 0..NUM_REPETITIONS {
+      bencher::black_box(llr_cse_pre(rng.gen_range(0, MAX_COOCCURRENCES),
+                                 rng.gen_range(0, MAX_OBSERVATIONS),
+                                 rng.gen_range(0, MAX_OBSERVATIONS),
+                                 rng.next_u64(),
+                                 &logs));
+    }
+  })
+}
 
 fn bench_llr_cse(bench: &mut Bencher) {
 
@@ -61,8 +124,10 @@ fn bench_llr_cse(bench: &mut Bencher) {
 
   bench.iter(|| {
     for _ in 0..NUM_REPETITIONS {
-      bencher::black_box(llr_cse(rng.next_u32() as u64, rng.next_u32() as u64,
-                                 rng.next_u32() as u64, rng.next_u32() as u64));
+      bencher::black_box(llr_cse(rng.gen_range(0, MAX_COOCCURRENCES),
+                                 rng.gen_range(0, MAX_OBSERVATIONS),
+                                 rng.gen_range(0, MAX_OBSERVATIONS),
+                                 rng.next_u64()));
     }
   })
 }
@@ -73,11 +138,13 @@ fn bench_llr_no_cse(bench: &mut Bencher) {
 
   bench.iter(|| {
     for _ in 0..NUM_REPETITIONS {
-      bencher::black_box(llr_no_cse(rng.next_u32() as u64, rng.next_u32() as u64,
-                                    rng.next_u32() as u64, rng.next_u32() as u64));
+      bencher::black_box(llr_no_cse(rng.gen_range(0, MAX_COOCCURRENCES),
+                                    rng.gen_range(0, MAX_OBSERVATIONS),
+                                    rng.gen_range(0, MAX_OBSERVATIONS),
+                                    rng.next_u64()));
     }
   })
 }
 
-benchmark_group!(benches, bench_llr_cse, bench_llr_no_cse);
+benchmark_group!(benches, bench_llr_no_cse, bench_llr_cse, bench_llr_cse_pre);
 benchmark_main!(benches);
